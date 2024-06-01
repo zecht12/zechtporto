62920150285ab6cb0f42d13ebd4ea3ede618582b
zecht12 <gilangprimae13@gmail.com>
2024-06-01 15:58:05 +0700

    adding social media to my zechtporto

--- components/Socials.tsx
+++ components/Socials.tsx
@@ -4,19 +4,16 @@ import { RiInstagramLine, RiFacebookLine, RiThreadsLine, RiTwitterLine, RiWhatsa
 const Socials = () => {
     return(
         <div className="flex items-center gap-x-4 text-lg">
-            <Link href="/" className="hover:text-accent transition-all duration-300">
+            <Link href="https://wa.me/6285865130221" className="hover:text-accent transition-all duration-300">
                 <RiWhatsappLine />
             </Link>
-            <Link href="/" className="hover:text-accent transition-all duration-300">
-                <RiFacebookLine />
-            </Link>
-            <Link href="/" className="hover:text-accent transition-all duration-300">
+            <Link href="https://x.com/ertansyah?t=owfBavdYd1mmUaizqe8D8Q&s=09" className="hover:text-accent transition-all duration-300">
                 <RiTwitterLine />
             </Link>
-            <Link href="/" className="hover:text-accent transition-all duration-300">
+            <Link href="https://www.threads.net/@gilangprimae" className="hover:text-accent transition-all duration-300">
                 <RiThreadsLine />
             </Link>
-            <Link href="/" className="hover:text-accent transition-all duration-300">
+            <Link href="https://www.instagram.com/gilangprimae?igsh=MTBsejU5Z3dwMmsyOQ==" className="hover:text-accent transition-all duration-300">
                 <RiInstagramLine />
             </Link>
         </div>
